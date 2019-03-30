e6e7e62dc1d3cf7809975492c881439fc24c8bf7
Micah Adams <micah.shawn.adams@gmail.com>
2019-03-29 18:57:19 -0600

    Initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,21 @@ import React from 'react';
 class App extends React.Component {
 
     render() {
-        return(
-            <div className="">
-                <h1>Foo</h1>
+        return (
+            <div className="wrapper">
+                <header className="bg-green-dark">Header</header>
+                    <div className="arrow bg-red-dark">
+                        <p>Left Arrrow</p>
+                    </div>
+                <main>
+                    <div className="image bg-indigo-dark">
+                        <p>Honkin' Image</p>
+                    </div>
+                </main>
+                <div className="arrow bg-red-dark">
+                    <p>Right Arrow</p>
+                </div>
+                <footer className="bg-yellow-dark">Footer</footer>
             </div>
         )
     }
